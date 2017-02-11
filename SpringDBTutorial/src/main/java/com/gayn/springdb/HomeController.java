2e62e96ed95fc12fe158bcdd1c0f677d1a64e8d7
gayanjeewantha2@gmail.com <gayanjeewantha2@gmail.com>
2017-02-11 23:16:35 +0530

    version 1.1

--- SpringDBTutorial/src/main/java/com/gayn/springdb/HomeController.java
+++ SpringDBTutorial/src/main/java/com/gayn/springdb/HomeController.java
@@ -50,6 +50,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		System.out.println("Hello this is test version");
 		
 		return "home";
 	}
